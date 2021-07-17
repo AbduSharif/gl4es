fe7cadb0519fbfc95763daab751ee60033c625c8
AbduSharif <57298920+AbduSharif@users.noreply.github.com>
2021-07-17 04:57:29 +0200

    add psa - Sisah

--- src/gl/init.c
+++ src/gl/init.c
@@ -668,6 +668,7 @@ void initialize_gl4es() {
         env(LIBGL_NOPSA, globals4es.nopsa, "Don't use PrecompiledShaderArchive");
         if(globals4es.nopsa==0) {
             cwd[0]='\0';
+/*
             // TODO: What to do on ANDROID and EMSCRIPTEN?
 #ifdef __linux__
             const char* home = GetEnvVar("HOME");
@@ -678,8 +679,12 @@ void initialize_gl4es() {
 #elif defined AMIGAOS4
             strcpy(cwd, "PROGDIR:");
 #endif
+*/
+            strcpy(cwd, "/storage/emulated/0/omw_nightly/");
+
             if(strlen(cwd)) {
                 strcat(cwd, ".gl4es.psa");
+                SHUT_LOGD("Shader archive file: %s\n", cwd);
                 fpe_InitPSA(cwd);
                 fpe_readPSA();
             }
